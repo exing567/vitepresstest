5f41fc051e990924f81310ff4fb52338082ed4ed
exing567 <exing567@163.com>
2025-12-22 21:20:08 +0800

    fix

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,13 @@
 import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
+import PagefindSearch from './components/PagefindSearch.vue'
 
 export default {
   ...DefaultTheme,
   Layout() {
-    return h(DefaultTheme.Layout)
+    return h(DefaultTheme.Layout, null, {
+      'nav-bar-content-after': () => h(PagefindSearch),
+      'nav-bar-title-after': () => h('span', { style: 'margin-left:8px;color:red;font-weight:700' }, 'TEST')
+    })
   }
 }
